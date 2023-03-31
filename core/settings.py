9676931f3bf5daf76b1684bc2dad3c91e73d0e24
Oroboros <diego-siebra@hotmail.com>
2023-03-31 14:00:03 -0300

    admin realms

--- core/settings.py
+++ core/settings.py
@@ -41,9 +41,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'helper',
+    'character',
     'geography.apps.GeographyConfig',
     'geopolitic.apps.GeopoliticConfig',
-    'character',
     'murim',
                     
 ]
--- murim/admin.py
+++ murim/admin.py
@@ -3,3 +3,4 @@ from . import  models
 # Register your models here.
 
 admin.site.register(models.Atribute)
+admin.site.register(models.Realms)
